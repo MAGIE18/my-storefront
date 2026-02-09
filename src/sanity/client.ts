0134bb49677631d89e6ae3ae213016ae699e8cf1
MAGIE18 <magietiwari01@gmail.com>
2026-02-10 02:45:10 +0530

    fix: hardcode project id for successful build

--- src/sanity/client.ts
+++ src/sanity/client.ts
@@ -1,8 +1,9 @@
 import { createClient } from "next-sanity";
 
+// This version checks for the variable and only uses it if it's there
 export const client = createClient({
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || "hdj3mcdy", // Replace p5k92s1a with your ID as a default
+  dataset: "production",
   apiVersion: "2024-01-01",
   useCdn: false,
 });
